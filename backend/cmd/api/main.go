ccb57a0a33102c2e856a3e419afcd024e804ea0b
narayan-mindfire <narayanp@mindfiresolutions.com>
2026-08-17 13:33:23 +0530

    feat(db): add job models, postgres connection pool and embedded auto-migration schema

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-	"github.com/narayan-mindfire/data-processor/backend/pkg/errors"
 	"github.com/narayan-mindfire/data-processor/backend/pkg/logger"
+	"github.com/narayan-mindfire/data-processor/backend/internal/store"
+	"os"
 )
 
 func main() {
 	logger.InitLogger()
+	dbHost := getEnv("DB_HOST", "localhost")
+	dbPort := getEnv("DB_PORT", "5432")
+	dbUser := getEnv("DB_USER", "postgres")
+	dbPass := getEnv("DB_PASS", "postgrespassword")
+	dbName := getEnv("DB_NAME", "dataprocessor")
 
-	logger.Log.Info("Data Processor API Server Initialized", "port", 8080)
+	db, err := store.NewDB(dbHost, dbPort, dbUser, dbPass, dbName)
+	if err != nil {
+		logger.Log.Error("failed to connect to the database", "error", err)
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	logger.Log.Info("Data Processor API Server Initialized and Database Connected", "port", 8080)
+}
+
+func getEnv(key, defaultValue string) string {
+		if value := os.Getenv(key); value != "" {
+			return value
+		}
+		return defaultValue
 }
---
+++ backend/internal/store/db.go
@@ -0,0 +1,62 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"embed"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq"
+	"github.com/narayan-mindfire/data-processor/backend/pkg/logger"
+)
+//go:embed migrations/*.sql
+var migrationFiles embed.FS
+
+type DB struct {
+	*sql.DB
+}
+
+// NewDB initializes the connection pool and runs migrations
+func NewDB(host, port, user, password, dbname string) (*DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+
+	// Connection Pool Best Practices
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	// Verify connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	storeDB := &DB{DB: db}
+
+	// Run Auto-Migrations
+	if err := storeDB.runMigrations(context.Background()); err != nil {
+		return nil, fmt.Errorf("failed to run migrations: %w", err)
+	}
+	logger.Log.Info("PostgreSQL connected and migrations applied successfully")
+	return storeDB, nil
+}
+
+// runMigrations reads the embedded SQL file and executes it
+func (db *DB) runMigrations(ctx context.Context) error {
+	sqlBytes, err := migrationFiles.ReadFile("migrations/001_init.sql")
+	if err != nil {
+		return fmt.Errorf("could not read migration file: %w", err)
+	}
+
+	_, err = db.ExecContext(ctx, string(sqlBytes))
+	return err
+}
